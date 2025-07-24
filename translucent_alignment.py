02b0948ad75c0b3a205bdd19eddf88b7aa7a9e0b
Christopher Schwanen <142578248+christopher-schwanen@users.noreply.github.com>
2025-07-24 20:36:37 +0200

    Added option for activity weights to align function

--- translucent_alignment.py
+++ translucent_alignment.py
@@ -4,11 +4,11 @@ from pm4py.objects.log.obj import Trace
 from translucent_reachability_graph import TranslucentReachabilityGraph, TranslucentAlignmentStateGraph
 
 
-def align(translucent_trace: Trace, translucent_reachability_graph: TranslucentReachabilityGraph, ignore_translucent: bool = False):
+def align(translucent_trace: Trace, translucent_reachability_graph: TranslucentReachabilityGraph, ignore_translucent: bool = False, activity_weights: dict[str, float] | None = None):
     # Check if the trace is enriched with translucent information
     if not all('enabled' in event for event in translucent_trace):
         raise ValueError("The trace is not enriched with translucent information")
-    return TranslucentAlignmentStateGraph(translucent_reachability_graph, translucent_trace).get_optimal_alignment(ignore_translucent=ignore_translucent)
+    return TranslucentAlignmentStateGraph(translucent_reachability_graph, translucent_trace, activity_weights).get_optimal_alignment(ignore_translucent=ignore_translucent)
 
 
 def align_to_petri_net(translucent_trace: Trace, accepting_petri_net: tuple[PetriNet, Marking, Marking], ignore_translucent: bool = False):
